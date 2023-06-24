86578657b8c6fca06ff64fc069734e5799acd5c5
asiifdev <asiif.anwar3@gmail.com>
2023-06-24 21:27:07 +0700

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Str;
 use App\Models\DashboardLayout;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Route;
-use PhpParser\Builder\Class_;
+use Illuminate\Support\Facades\Schema;
 
 if (!function_exists('moneyFormat')) {
     /**
@@ -30,7 +30,10 @@ if (!function_exists('moneyFormat')) {
  */
 function getIdentity()
 {
-    $data = Identity::first();
+    $data = [];
+    if (Schema::hasTable('identities')) {
+        $data = Identity::first();
+    }
     return $data;
 }
 
@@ -44,12 +47,14 @@ function getIdentity()
 function getRoles($id)
 {
     $data = [];
-    if ($id) {
-        $user = User::find($id);
-        $role_id = $user->roles->first()->id;
-        $data = Role::find($role_id);
-    } else {
-        $data = Role::all();
+    if (Schema::hasTable('identities')) {
+        if ($id) {
+            $user = User::find($id);
+            $role_id = $user->roles->first()->id;
+            $data = Role::find($role_id);
+        } else {
+            $data = Role::all();
+        }
     }
     return $data;
 }
@@ -64,11 +69,13 @@ function getRoles($id)
 function getCurrentMenu()
 {
     $data = [];
-    $url = "/" . request()->path();
-    if($url == "//"){
-        $url = "/";
+    if (Schema::hasTable('identities')) {
+        $url = "/" . request()->path();
+        if ($url == "//") {
+            $url = "/";
+        }
+        $data = Menu::where('url', $url)->first();
     }
-    $data = Menu::where('url', $url)->first();
     return $data;
 }
 
@@ -81,7 +88,10 @@ function getCurrentMenu()
  */
 function getAllMenu()
 {
-    $data = Menu::with('icons')->get();
+    $data = [];
+    if (Schema::hasTable('identities')) {
+        $data = Menu::with('icons')->get();
+    }
     return $data;
 }
 
@@ -94,9 +104,11 @@ function getAllMenu()
 function getNameSpace()
 {
     $use = "use";
-    $menu = Menu::all();
-    foreach ($menu as $item) {
-        $use .= " " . $item->pathClass . "; ";
+    if (Schema::hasTable('identities')) {
+        $menu = Menu::all();
+        foreach ($menu as $item) {
+            $use .= " " . $item->pathClass . "; ";
+        }
     }
     return $use;
 }
@@ -108,9 +120,11 @@ function getNameSpace()
  */
 function getRouting()
 {
-    $menu = Menu::all();
-    foreach ($menu as $item) {
-        $route = Route::get($item->url, $item->pathClass)->name($item->slug);
+    if (Schema::hasTable('identities')) {
+        $menu = Menu::all();
+        foreach ($menu as $item) {
+            $route = Route::get($item->url, $item->pathClass)->name($item->slug);
+        }
     }
     return $route;
 }
@@ -122,23 +136,24 @@ function getRouting()
  */
 function getLayout()
 {
-    $layout = DashboardLayout::first();
-    if ($layout) {
-        $array = [];
-        $data = [];
-        $array['placement'] = $layout->placement;
-        $array['behaviour'] = $layout->behaviour;
-        $array['radius'] = $layout->radius;
-        $array['color'] = $layout->color;
-        $array['navcolor'] = $layout->navcolor;
-        $data['attributes'] = (object) $array;
-        $data['layout'] = $layout->layout;
-        $data['footer'] = $layout->footer;
-        $data['storagePrefix'] = Str::slug(getIdentity()->name);
-        $data['showSettings'] = false;
-        $html = ["override" => json_encode($data)];
-    } else {
-        $html = null;
+    $html = null;
+    if (Schema::hasTable('identities')) {
+        $layout = DashboardLayout::first();
+        if ($layout) {
+            $array = [];
+            $data = [];
+            $array['placement'] = $layout->placement;
+            $array['behaviour'] = $layout->behaviour;
+            $array['radius'] = $layout->radius;
+            $array['color'] = $layout->color;
+            $array['navcolor'] = $layout->navcolor;
+            $data['attributes'] = (object) $array;
+            $data['layout'] = $layout->layout;
+            $data['footer'] = $layout->footer;
+            $data['storagePrefix'] = Str::slug(getIdentity()->name);
+            $data['showSettings'] = false;
+            $html = ["override" => json_encode($data)];
+        }
     }
 
     return $html;
